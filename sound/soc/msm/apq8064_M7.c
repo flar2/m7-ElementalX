6c5d1e1e1d5412417bae837e18c5dd7eaecd39d1
flar2 <asegaert@gmail.com>
2013-06-18 23:01:41 -0400

    update to latest HTC sources

--- sound/soc/msm/apq8064_M7.c
+++ sound/soc/msm/apq8064_M7.c
@@ -57,7 +57,10 @@
 #define TOP_SPK_AMP_POS		0x4
 #define TOP_SPK_AMP_NEG		0x8
 #define TOP_SPK_AMP		0x10
-
+#define HS_AMP_POS		0x20
+#define HS_AMP_NEG		0x40
+#define RCV_AMP_POS		0x80
+#define RCV_AMP_NEG		0x100
 
 #define GPIO_AUX_PCM_DOUT 43
 #define GPIO_AUX_PCM_DIN 44
@@ -78,10 +81,20 @@
 #define TABLA_MBHC_DEF_BUTTONS 8
 #define TABLA_MBHC_DEF_RLOADS 5
 #define HAC_PAMP_GPIO	6
+
+#ifdef CONFIG_MACH_DUMMY
+#define RCV_PAMP_PMGPIO	24
+#else
 #define RCV_PAMP_GPIO    67
+#endif
+
+#ifdef CONFIG_MACH_DUMMY
+#define RCV_SPK_SEL_PMGPIO    5
+#else
 #define RCV_SPK_SEL_PMGPIO    24
+#endif
+
 static int msm_hac_control;
-static int msm_rcv_control;
 static int aux_pcm_open = 0;
 enum {
 	SLIM_1_RX_1 = 145, 
@@ -101,9 +114,10 @@ enum {
 };
 
 static int msm_spk_control;
-static int msm_spk9887mute_control = 0;
 static int msm_ext_bottom_spk_pamp;
 static int msm_ext_top_spk_pamp;
+static int msm_hs_pamp;
+static int msm_rcv_pamp;
 static int msm_slim_0_rx_ch = 1;
 static int msm_slim_0_tx_ch = 1;
 static struct clk *mi2s_rx_osr_clk;
@@ -149,6 +163,30 @@ static struct tabla_mbhc_config mbhc_cfg = {
 	.gpio_level_insert = 1,
 };
 
+static inline int param_is_mask(int p)
+{
+	return ((p >= SNDRV_PCM_HW_PARAM_FIRST_MASK) &&
+		(p <= SNDRV_PCM_HW_PARAM_LAST_MASK));
+}
+
+static inline struct snd_mask *param_to_mask(struct snd_pcm_hw_params *p, int n)
+{
+	return &(p->masks[n - SNDRV_PCM_HW_PARAM_FIRST_MASK]);
+}
+
+static void param_set_mask(struct snd_pcm_hw_params *p, int n, unsigned bit)
+{
+	if (bit >= SNDRV_MASK_MAX)
+		return;
+	if (param_is_mask(n)) {
+		struct snd_mask *m = param_to_mask(p, n);
+		m->bits[0] = 0;
+		m->bits[1] = 0;
+		m->bits[bit >> 5] |= (1 << (bit & 31));
+	}
+}
+
+
 static struct mutex cdc_mclk_mutex;
 static struct mutex aux_pcm_mutex;
 
@@ -180,8 +218,9 @@ static int msm8960_mi2s_hw_params(struct snd_pcm_substream *substream,
 	int rate = params_rate(params);
 	int bit_clk_set = 0;
 
+	
 	if (substream->stream == SNDRV_PCM_STREAM_PLAYBACK) {
-		bit_clk_set = 12288000/(rate * 2 * 16);
+		bit_clk_set = 18432000/(rate * 2 * 24);
 		clk_set_rate(mi2s_rx_bit_clk, bit_clk_set);
 	}
 	return 1;
@@ -255,7 +294,8 @@ static int msm8960_mi2s_startup(struct snd_pcm_substream *substream)
 		configure_mi2s_rx_gpio();
 		mi2s_rx_osr_clk = clk_get(cpu_dai->dev, "osr_clk");
 		if (mi2s_rx_osr_clk) {
-			clk_set_rate(mi2s_rx_osr_clk, 12288000);
+			
+			clk_set_rate(mi2s_rx_osr_clk, 18432000);
 			clk_prepare_enable(mi2s_rx_osr_clk);
 		}
 		mi2s_rx_bit_clk = clk_get(cpu_dai->dev, "bit_clk");
@@ -439,7 +479,63 @@ static struct snd_soc_ops msm8960_i2s_be_ops = {
 
 static void msm_ext_spk_power_amp_on(u32 spk)
 {
-	if (spk & (BOTTOM_SPK_AMP_POS | BOTTOM_SPK_AMP_NEG)) {
+	if (spk & (RCV_AMP_POS | RCV_AMP_NEG)) {
+		if ((msm_rcv_pamp & RCV_AMP_POS) &&
+			(msm_rcv_pamp & RCV_AMP_NEG)) {
+
+			pr_debug("%s() HS Ampl already "
+				"turned on. spk = 0x%08x\n", __func__, spk);
+			return;
+		}
+
+		msm_rcv_pamp |= spk;
+
+		if ((msm_rcv_pamp & RCV_AMP_POS) &&
+			(msm_rcv_pamp & RCV_AMP_NEG)) {
+
+
+			pr_info("rcv amp on++");
+#ifdef CONFIG_MACH_DUMMY
+			gpio_direction_output(PM8921_GPIO_PM_TO_SYS(RCV_PAMP_PMGPIO), 1);
+#else
+			gpio_direction_output(RCV_PAMP_GPIO, 1);
+#endif
+			gpio_direction_output(PM8921_GPIO_PM_TO_SYS(RCV_SPK_SEL_PMGPIO), 1);
+			pr_info("rcv amp on--");
+
+			pr_debug("%s: slepping 4 ms after turning on external "
+				" Bottom Speaker Ampl\n", __func__);
+			usleep_range(4000, 4000);
+		}
+	} else if (spk & (HS_AMP_POS | HS_AMP_NEG)) {
+
+		if ((msm_hs_pamp & HS_AMP_POS) &&
+			(msm_hs_pamp & HS_AMP_NEG)) {
+
+			pr_debug("%s() HS Ampl already "
+				"turned on. spk = 0x%08x\n", __func__, spk);
+			return;
+		}
+
+		msm_hs_pamp |= spk;
+
+		if ((msm_hs_pamp & HS_AMP_POS) &&
+			(msm_hs_pamp & HS_AMP_NEG)) {
+			
+			pr_info("hs amp on++");
+                        if(query_tpa6185()) {
+                            gpio_direction_output(PM8921_GPIO_PM_TO_SYS(10), 1);
+			    set_handset_amp(1);
+                        }
+
+                        if(query_rt5501())
+                            set_rt5501_amp(1);
+			pr_info("hs amp on--");
+			pr_debug("%s: slepping 4 ms after turning on external "
+				" Bottom Speaker Ampl\n", __func__);
+			usleep_range(4000, 4000);
+		}
+	} else if (spk & (BOTTOM_SPK_AMP_POS | BOTTOM_SPK_AMP_NEG)) {
 
 		if ((msm_ext_bottom_spk_pamp & BOTTOM_SPK_AMP_POS) &&
 			(msm_ext_bottom_spk_pamp & BOTTOM_SPK_AMP_NEG)) {
@@ -453,7 +549,7 @@ static void msm_ext_spk_power_amp_on(u32 spk)
 
 		if ((msm_ext_bottom_spk_pamp & BOTTOM_SPK_AMP_POS) &&
 			(msm_ext_bottom_spk_pamp & BOTTOM_SPK_AMP_NEG)) {
-
+#if 0
 			
 			pr_info("hs amp on++");
                         if(query_tpa6185()) {
@@ -464,6 +560,7 @@ static void msm_ext_spk_power_amp_on(u32 spk)
                         if(query_rt5501())
                             set_rt5501_amp(1);
 			pr_info("hs amp on--");
+#endif
 			pr_debug("%s: slepping 4 ms after turning on external "
 				" Bottom Speaker Ampl\n", __func__);
 			usleep_range(4000, 4000);
@@ -503,9 +600,28 @@ static void msm_ext_spk_power_amp_on(u32 spk)
 
 static void msm_ext_spk_power_amp_off(u32 spk)
 {
-	if (spk & (BOTTOM_SPK_AMP_POS | BOTTOM_SPK_AMP_NEG)) {
+	if (spk & (RCV_AMP_POS | RCV_AMP_NEG)) {
+		if (!msm_rcv_pamp)
+			return;
 
-		if (!msm_ext_bottom_spk_pamp)
+
+		pr_info("rcv amp off ++");
+#ifdef CONFIG_MACH_DUMMY
+		gpio_direction_output(PM8921_GPIO_PM_TO_SYS(RCV_PAMP_PMGPIO), 0);
+#else
+		gpio_direction_output(RCV_PAMP_GPIO, 0);
+#endif
+		gpio_direction_output(PM8921_GPIO_PM_TO_SYS(RCV_SPK_SEL_PMGPIO), 0);
+		pr_info("rcv amp off --");
+
+		msm_rcv_pamp = 0;
+
+		pr_debug("%s: sleeping 4 ms after turning off external Bottom"
+			" Speaker Ampl\n", __func__);
+
+		usleep_range(4000, 4000);
+	} else if (spk & (HS_AMP_POS | HS_AMP_NEG)) {
+		if (!msm_hs_pamp)
 			return;
 
 		
@@ -519,6 +635,28 @@ static void msm_ext_spk_power_amp_off(u32 spk)
                     set_rt5501_amp(0);
 		pr_info("hs amp off --");
 
+		msm_hs_pamp = 0;
+
+		pr_debug("%s: sleeping 4 ms after turning off external Bottom"
+			" Speaker Ampl\n", __func__);
+
+		usleep_range(4000, 4000);
+	} else if (spk & (BOTTOM_SPK_AMP_POS | BOTTOM_SPK_AMP_NEG)) {
+
+		if (!msm_ext_bottom_spk_pamp)
+			return;
+#if 0
+		
+		pr_info("hs amp off ++");
+                if(query_tpa6185()) {
+		    set_handset_amp(0);
+                    gpio_direction_output(PM8921_GPIO_PM_TO_SYS(10), 0);
+                }
+
+                if(query_rt5501())
+                    set_rt5501_amp(0);
+		pr_info("hs amp off --");
+#endif
 		msm_ext_bottom_spk_pamp = 0;
 
 		pr_debug("%s: sleeping 4 ms after turning off external Bottom"
@@ -593,7 +731,7 @@ static int msm_set_hac(struct snd_kcontrol *kcontrol,
 		}
 	return 1;
 }
-
+#if 0
 static int msm_get_rcv_amp(struct snd_kcontrol *kcontrol,
 	struct snd_ctl_elem_value *ucontrol)
 {
@@ -633,6 +771,7 @@ static int msm_set_rcv_amp(struct snd_kcontrol *kcontrol,
 		}
 	return 1;
 }
+#endif
 
 static void msm_ext_control(struct snd_soc_codec *codec)
 {
@@ -644,11 +783,19 @@ static void msm_ext_control(struct snd_soc_codec *codec)
 		snd_soc_dapm_enable_pin(dapm, "Ext Spk Bottom Neg");
 		snd_soc_dapm_enable_pin(dapm, "Ext Spk Top Pos");
 		snd_soc_dapm_enable_pin(dapm, "Ext Spk Top Neg");
+		snd_soc_dapm_enable_pin(dapm, "Ext Hs Pos");
+		snd_soc_dapm_enable_pin(dapm, "Ext Hs Neg");
+		snd_soc_dapm_enable_pin(dapm, "Ext Rcv Pos");
+		snd_soc_dapm_enable_pin(dapm, "Ext Rcv Neg");
 	} else {
 		snd_soc_dapm_disable_pin(dapm, "Ext Spk Bottom Pos");
 		snd_soc_dapm_disable_pin(dapm, "Ext Spk Bottom Neg");
 		snd_soc_dapm_disable_pin(dapm, "Ext Spk Top Pos");
 		snd_soc_dapm_disable_pin(dapm, "Ext Spk Top Neg");
+		snd_soc_dapm_disable_pin(dapm, "Ext Hs Pos");
+		snd_soc_dapm_disable_pin(dapm, "Ext Hs Neg");
+		snd_soc_dapm_disable_pin(dapm, "Ext Rcv Pos");
+		snd_soc_dapm_disable_pin(dapm, "Ext Rcv Neg");
 	}
 
 	snd_soc_dapm_sync(dapm);
@@ -674,36 +821,10 @@ static int msm_set_spk(struct snd_kcontrol *kcontrol,
 	msm_ext_control(codec);
 	return 1;
 }
-
-static int msm_get_spk9887mute(struct snd_kcontrol *kcontrol,
-       struct snd_ctl_elem_value *ucontrol) {
-       pr_debug("%s: msm_spk9887_control = %d", __func__, msm_spk9887mute_control);
-       ucontrol->value.integer.value[0] = msm_spk9887mute_control;
-       return 0;
-}
-static int msm_set_spk9887mute(struct snd_kcontrol *kcontrol,
-       struct snd_ctl_elem_value *ucontrol) {
-
-       
-       
-
-       msm_spk9887mute_control = ucontrol->value.integer.value[0];
-       pr_info("@@## %s() %d\n", __func__,msm_spk9887mute_control);
-       if (msm_spk9887mute_control)
-       {
-               msleep(200);
-               set_tfa9887_spkamp(0, 0);
-       }
-       else
-               set_tfa9887_spkamp(1, 0);
-
-       return 1;
-}
-
 static int msm_spkramp_event(struct snd_soc_dapm_widget *w,
 	struct snd_kcontrol *k, int event)
 {
-	pr_debug("%s() %x\n", __func__, SND_SOC_DAPM_EVENT_ON(event));
+	pr_info("%s() wname %s %x\n", __func__, w->name,SND_SOC_DAPM_EVENT_ON(event));
 
 	if (SND_SOC_DAPM_EVENT_ON(event)) {
 		if (!strncmp(w->name, "Ext Spk Bottom Pos", 18))
@@ -714,6 +835,14 @@ static int msm_spkramp_event(struct snd_soc_dapm_widget *w,
 			msm_ext_spk_power_amp_on(TOP_SPK_AMP_POS);
 		else if  (!strncmp(w->name, "Ext Spk Top Neg", 15))
 			msm_ext_spk_power_amp_on(TOP_SPK_AMP_NEG);
+		else if (!strncmp(w->name, "Ext Hs Pos", 10))
+			msm_ext_spk_power_amp_on(HS_AMP_POS);
+		else if  (!strncmp(w->name, "Ext Hs Neg", 10))
+			msm_ext_spk_power_amp_on(HS_AMP_NEG);
+		else if (!strncmp(w->name, "Ext Rcv Pos", 11))
+			msm_ext_spk_power_amp_on(RCV_AMP_POS);
+		else if  (!strncmp(w->name, "Ext Rcv Neg", 11))
+			msm_ext_spk_power_amp_on(RCV_AMP_NEG);
 		else if  (!strncmp(w->name, "Ext Spk Top", 12))
 			msm_ext_spk_power_amp_on(TOP_SPK_AMP);
 		else {
@@ -731,6 +860,14 @@ static int msm_spkramp_event(struct snd_soc_dapm_widget *w,
 			msm_ext_spk_power_amp_off(TOP_SPK_AMP_POS);
 		else if  (!strncmp(w->name, "Ext Spk Top Neg", 15))
 			msm_ext_spk_power_amp_off(TOP_SPK_AMP_NEG);
+		else if (!strncmp(w->name, "Ext Hs Pos", 10))
+			msm_ext_spk_power_amp_off(HS_AMP_POS);
+		else if  (!strncmp(w->name, "Ext Hs Neg", 10))
+			msm_ext_spk_power_amp_off(HS_AMP_NEG);
+		else if (!strncmp(w->name, "Ext Rcv Pos", 11))
+			msm_ext_spk_power_amp_off(RCV_AMP_POS);
+		else if  (!strncmp(w->name, "Ext Rcv Neg", 11))
+			msm_ext_spk_power_amp_off(RCV_AMP_NEG);
 		else if  (!strncmp(w->name, "Ext Spk Top", 12))
 			msm_ext_spk_power_amp_off(TOP_SPK_AMP);
 		else {
@@ -829,6 +966,21 @@ static int msm_mclk_event(struct snd_soc_dapm_widget *w,
 	return 0;
 }
 
+static const struct snd_kcontrol_new extspk_switch_controls =
+	SOC_DAPM_SINGLE("Switch", 0, 0, 1, 0);
+
+static const struct snd_kcontrol_new earamp_switch_controls =
+	SOC_DAPM_SINGLE("Switch", 0, 0, 1, 0);
+
+static const struct snd_kcontrol_new spkamp_switch_controls =
+	SOC_DAPM_SINGLE("Switch", 0, 0, 1, 0);
+
+static const struct snd_kcontrol_new hsamp_switch_controls =
+	SOC_DAPM_SINGLE("Switch", 0, 0, 1, 0);
+
+static const struct snd_kcontrol_new rcvamp_switch_controls =
+	SOC_DAPM_SINGLE("Switch", 0, 0, 1, 0);
+
 static const struct snd_soc_dapm_widget apq8064_dapm_widgets[] = {
 
 	SND_SOC_DAPM_SUPPLY("MCLK",  SND_SOC_NOPM, 0, 0,
@@ -841,6 +993,11 @@ static const struct snd_soc_dapm_widget apq8064_dapm_widgets[] = {
 	SND_SOC_DAPM_SPK("Ext Spk Top Neg", msm_spkramp_event),
 	SND_SOC_DAPM_SPK("Ext Spk Top", msm_spkramp_event),
 
+	SND_SOC_DAPM_SPK("Ext Hs Pos", msm_spkramp_event),
+	SND_SOC_DAPM_SPK("Ext Hs Neg", msm_spkramp_event),
+
+	SND_SOC_DAPM_SPK("Ext Rcv Pos", msm_spkramp_event),
+	SND_SOC_DAPM_SPK("Ext Rcv Neg", msm_spkramp_event),
 	
 	SND_SOC_DAPM_MIC("Analog mic7", NULL),
 
@@ -856,6 +1013,11 @@ static const struct snd_soc_dapm_widget apq8064_dapm_widgets[] = {
 	SND_SOC_DAPM_MIC("Digital Mic5", NULL),
 	SND_SOC_DAPM_MIC("Digital Mic6", NULL),
 	SND_SOC_DAPM_MIXER("Lineout Mixer", SND_SOC_NOPM, 0, 0, NULL, 0),
+	SND_SOC_DAPM_MIXER("SPK AMP EN", SND_SOC_NOPM, 0, 0, &spkamp_switch_controls, 1),
+	SND_SOC_DAPM_MIXER("HAC AMP EN", SND_SOC_NOPM, 0, 0, &earamp_switch_controls, 1),
+	SND_SOC_DAPM_MIXER("DOCK AMP EN", SND_SOC_NOPM, 0, 0, &extspk_switch_controls, 1),
+	SND_SOC_DAPM_MIXER("HS AMP EN", SND_SOC_NOPM, 0, 0, &hsamp_switch_controls, 1),
+	SND_SOC_DAPM_MIXER("RCV AMP EN", SND_SOC_NOPM, 0, 0, &rcvamp_switch_controls, 1),
 };
 
 static const struct snd_soc_dapm_route apq8064_common_audio_map[] = {
@@ -873,6 +1035,19 @@ static const struct snd_soc_dapm_route apq8064_common_audio_map[] = {
 	{"Ext Spk Top Neg", NULL, "LINEOUT4"},
 	{"Ext Spk Top", NULL, "LINEOUT5"},
 
+
+	
+	{"Ext Hs Pos", NULL, "HS AMP EN"},
+	{"Ext Hs Neg", NULL, "HS AMP EN"},
+	{"HS AMP EN", "Switch", "Lineout Mixer"},
+
+	
+	{"Ext Rcv Pos", NULL, "RCV AMP EN"},
+	{"Ext Rcv Neg", NULL, "RCV AMP EN"},
+	{"RCV AMP EN", "Switch", "Lineout Mixer"},
+
+	{"Lineout Mixer", NULL, "LINEOUT3"},
+	{"Lineout Mixer", NULL, "LINEOUT1"},
 	
 	{"AMIC1", NULL, "MIC BIAS1 External"},
 	{"MIC BIAS1 External", NULL, "Analog mic7"},
@@ -1073,10 +1248,10 @@ static const struct snd_kcontrol_new tabla_msm_controls[] = {
 		msm_slim_3_rx_ch_get, msm_slim_3_rx_ch_put),
 	SOC_ENUM_EXT("HAC AMP EN", msm_enum[0], msm_get_hac,
 		msm_set_hac),
+#if 0
 	SOC_ENUM_EXT("RCV AMP EN", msm_enum[0], msm_get_rcv_amp,
 		msm_set_rcv_amp),
-    SOC_ENUM_EXT("Mute9887 Function", msm_enum[0],msm_get_spk9887mute,
-            msm_set_spk9887mute),
+#endif
 
 
 };
@@ -1384,6 +1559,10 @@ static int msm_audrx_init(struct snd_soc_pcm_runtime *rtd)
 	snd_soc_dapm_enable_pin(dapm, "Ext Spk Bottom Neg");
 	snd_soc_dapm_enable_pin(dapm, "Ext Spk Top Pos");
 	snd_soc_dapm_enable_pin(dapm, "Ext Spk Top Neg");
+	snd_soc_dapm_enable_pin(dapm, "Ext Hs Pos");
+	snd_soc_dapm_enable_pin(dapm, "Ext Hs Neg");
+	snd_soc_dapm_enable_pin(dapm, "Ext Rcv Pos");
+	snd_soc_dapm_enable_pin(dapm, "Ext Rcv Neg");
 
 	snd_soc_dapm_sync(dapm);
 
@@ -1416,7 +1595,31 @@ static int msm_slim_0_rx_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 	struct snd_interval *channels = hw_param_interval(params,
 			SNDRV_PCM_HW_PARAM_CHANNELS);
 
+	pr_debug("%s() Fixing the BE DAI format to 24bit\n", __func__);
+
+	param_set_mask(params, SNDRV_PCM_HW_PARAM_FORMAT,
+		SNDRV_PCM_FORMAT_S24_LE);
+
+	rate->min = rate->max = 48000;
+	channels->min = channels->max = msm_slim_0_rx_ch;
+
+	return 0;
+}
+
+static int msm_slim_0_stub_rx_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
+			struct snd_pcm_hw_params *params)
+{
+	struct snd_interval *rate = hw_param_interval(params,
+	SNDRV_PCM_HW_PARAM_RATE);
+
+	struct snd_interval *channels = hw_param_interval(params,
+			SNDRV_PCM_HW_PARAM_CHANNELS);
+
 	pr_debug("%s()\n", __func__);
+
+	param_set_mask(params, SNDRV_PCM_HW_PARAM_FORMAT,
+		SNDRV_PCM_FORMAT_S16_LE);
+
 	rate->min = rate->max = 48000;
 	channels->min = channels->max = msm_slim_0_rx_ch;
 
@@ -1449,6 +1652,10 @@ static int msm_slim_3_rx_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 			SNDRV_PCM_HW_PARAM_CHANNELS);
 
 	pr_debug("%s()\n", __func__);
+
+	param_set_mask(params, SNDRV_PCM_HW_PARAM_FORMAT,
+		SNDRV_PCM_FORMAT_S16_LE);
+
 	rate->min = rate->max = 48000;
 	channels->min = channels->max = msm_slim_3_rx_ch;
 
@@ -1484,6 +1691,10 @@ static int msm_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 	SNDRV_PCM_HW_PARAM_RATE);
 
 	pr_debug("%s()\n", __func__);
+
+	param_set_mask(params, SNDRV_PCM_HW_PARAM_FORMAT,
+		SNDRV_PCM_FORMAT_S16_LE);
+
 	rate->min = rate->max = 48000;
 
 	return 0;
@@ -1500,6 +1711,8 @@ static int msm_hdmi_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 
 	pr_debug("%s channels->min %u channels->max %u ()\n", __func__,
 			channels->min, channels->max);
+	param_set_mask(params, SNDRV_PCM_HW_PARAM_FORMAT,
+		SNDRV_PCM_FORMAT_S16_LE);
 
 	rate->min = rate->max = 48000;
 
@@ -1530,11 +1743,32 @@ static int msm_mi2s_rx_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 	struct snd_interval *channels = hw_param_interval(params,
 					SNDRV_PCM_HW_PARAM_CHANNELS);
 
+	pr_debug("%s() Fixing the BE DAI format to 24bit\n", __func__);
+
+	param_set_mask(params, SNDRV_PCM_HW_PARAM_FORMAT,
+		SNDRV_PCM_FORMAT_S24_LE);
+
 	rate->min = rate->max = 48000;
 	channels->min = channels->max = 1;
 #ifdef CONFIG_AMP_TFA9887L
 	channels->min = channels->max = 2;
 #endif
+
+	return 0;
+}
+
+static int msm_mi2s_tx_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
+			struct snd_pcm_hw_params *params)
+{
+	struct snd_interval *rate = hw_param_interval(params,
+	SNDRV_PCM_HW_PARAM_RATE);
+
+	pr_debug("%s()\n", __func__);
+
+	param_set_mask(params, SNDRV_PCM_HW_PARAM_FORMAT,
+		SNDRV_PCM_FORMAT_S24_LE);
+
+	rate->min = rate->max = 48000;
 
 	return 0;
 }
@@ -1579,7 +1813,11 @@ static int msm_auxpcm_be_params_fixup(struct snd_soc_pcm_runtime *rtd,
 					SNDRV_PCM_HW_PARAM_CHANNELS);
 
 	
+#ifdef CONFIG_BT_WBS_BRCM
+	rate->min = rate->max = 16000;
+#else
 	rate->min = rate->max = 8000;
+#endif
 	channels->min = channels->max = 1;
 
 	return 0;
@@ -1592,6 +1830,9 @@ static int msm_proxy_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 	SNDRV_PCM_HW_PARAM_RATE);
 
 	pr_debug("%s()\n", __func__);
+	param_set_mask(params, SNDRV_PCM_HW_PARAM_FORMAT,
+		SNDRV_PCM_FORMAT_S16_LE);
+
 	rate->min = rate->max = 48000;
 
 	return 0;
@@ -2019,7 +2260,7 @@ static struct snd_soc_dai_link msm_dai[] = {
 		.codec_name     = "msm-stub-codec.1",
 		.codec_dai_name = "msm-stub-tx",
 		.no_pcm = 1,
-		.be_hw_params_fixup = msm_be_hw_params_fixup,
+		.be_hw_params_fixup = msm_mi2s_tx_be_hw_params_fixup,
 		.be_id = MSM_BACKEND_DAI_MI2S_TX,
 		.ops = &msm8960_mi2s_be_ops,
 	},
@@ -2082,7 +2323,7 @@ static struct snd_soc_dai_link msm_dai[] = {
 		.codec_dai_name = "tabla_rx2",
 		.no_pcm = 1,
 		.be_id = MSM_BACKEND_DAI_EXTPROC_RX,
-		.be_hw_params_fixup = msm_slim_0_rx_be_hw_params_fixup,
+		.be_hw_params_fixup = msm_slim_0_stub_rx_be_hw_params_fixup,
 		.init = &msm_stubrx_init,
 		.ops = &msm_be_ops,
 		.ignore_pmdown_time = 1, 
@@ -2174,7 +2415,7 @@ static struct snd_soc_dai_link msm_dai[] = {
 		.codec_dai_name	= "tabla_tx3",
 		.no_pcm = 1,
 		.be_id = MSM_BACKEND_DAI_EXTPROC_EC_TX,
-		.be_hw_params_fixup = msm_slim_0_rx_be_hw_params_fixup,
+		.be_hw_params_fixup = msm_slim_0_stub_rx_be_hw_params_fixup,
 		.ops = &msm_be_ops,
 	},
 	{
@@ -2274,6 +2515,19 @@ static struct snd_soc_dai_link msm_dai[] = {
 		.ignore_suspend = 1,
 		.ignore_pmdown_time = 1, 
 		.be_id = MSM_FRONTEND_DAI_MULTIMEDIA8,
+	},
+	{
+		.name = "Compress Stub",
+		.stream_name = "Compress Stub",
+		.cpu_dai_name	= "MM_STUB",
+		.platform_name  = "msm-pcm-hostless",
+		.dynamic = 1,
+		.trigger = {SND_SOC_DPCM_TRIGGER_POST, SND_SOC_DPCM_TRIGGER_POST},
+		.no_host_mode = SND_SOC_DAI_LINK_NO_HOST,
+		.ignore_suspend = 1,
+		.ignore_pmdown_time = 1, 
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.codec_name = "snd-soc-dummy",
 	},
 	
 
